b816ede26d7fcec4e148e2849c26352ffee8bcd5
mazgutheng <35424920+mazgutheng@users.noreply.github.com>
2018-08-28 05:32:13 +0700

    Update ayu.py

--- ayu.py
+++ ayu.py
@@ -11,19 +11,6 @@ from bs4 import BeautifulSoup
 
 #===============================================================================================
 
-# -*- coding: utf-8 -*- 
-import LINEPY
-from LINEPY import *
-from akad.ttypes import *
-from multiprocessing import Pool, Process
-from time import sleep
-import pytz, datetime, pafy, time, timeit, random, sys, ast, re, os, json, subprocess, threading, string, codecs, requests, tweepy, ctypes, urllib, wikipedia
-from datetime import timedelta, date
-from datetime import datetime
-from bs4 import BeautifulSoup
-
-#===============================================================================================
-
 aditmadzs = LineClient(authToken='EwtsGPptYoJ1NhCDZAw0.voo4510XogOd5b1iDS++ya.J29YzMYNkYuVMSSZnk2U2MINZInbBqhf5m2WTU1+qDU')
 aditmadzs.log("Auth Token : " + str(aditmadzs.authToken))
 channel = LineChannel(aditmadzs)
